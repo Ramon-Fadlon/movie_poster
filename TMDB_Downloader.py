d7f36bf468ec364a13dfee8978bf2c6afdb89bf3
ramon <ramonfadlon@gmail.com>
2022-10-16 13:15:16 +0300

    get poster 16.10

--- TMDB_Downloader.py
+++ TMDB_Downloader.py
@@ -1,35 +1,58 @@
-import os
+from config import TMDB_key
 import requests
 import imdb
-from config import TMDB_key
-
+import os
 class tmdb_downloder:
-    def __init__(self):
+
+    def __init__(self,title):
+        self.title = title
+        self.api_response = None
         self.Config_Pattern = 'http://api.themoviedb.org/3/configuration?api_key={key}'
         self.KEY = TMDB_key
         self.url = self.Config_Pattern.format(key=self.KEY)
         self.r = requests.get(self.url)
         self.config = self.r.json()
-        self.secure_base_url = self.config['images']['secure_base_url']
+        self.base_url = self.config['images']['secure_base_url']
         self.sizes = self.config['images']['poster_sizes']
         self.max_size = 'original'
         self.IMG_PATTERN = 'http://api.themoviedb.org/3/movie/{imdbid}/images?api_key={key}'
-        self.get_imdb_id()
-        self.tmdb_posters()
-
-    def get_imdb_id(self, title):
-        ia = imdb.Cinemagoer()
-        movies = ia.search_movie(title)
-        imdbid = "tt" + movies[0].movieID
-        self.tmdb_posters(title,imdbid)
-
-    def tmdb_posters(self, title, count=None, outpath='.'):
-        urls = get_poster_url(self.imdbid)
-        if count is not None:
-            urls = urls[:count]
-        _download_image(title, urls, outpath)
 
 
+
+
+    def Get_Imbid(self):
+        ia = imdb.IMDb()
+        search = ia.search_movie(self.title)
+        self.imdb_id = "tt" + str(search[0].movieID)
+        r = requests.get(self.IMG_PATTERN.format(key=self.KEY, imdbid=self.imdb_id))
+        self.api_response = r.json()
+        return self.api_response
+
+    def Poster_Download(self):
+        posters = self.api_response['posters']
+        self.poster_urls = []
+        for poster in posters:
+            rel_path = poster['file_path']
+            self.url = "{0}{1}{2}".format(self.base_url, self.max_size, rel_path)
+            self.poster_urls.append(self.url)
+        return self.poster_urls
+
+    def Save_To(self):
+        r = requests.get(self.poster_urls[0])
+        filetype = r.headers['content-type'].split('/')[-1]
+        filename = '{0}.{1}'.format(self.title, filetype)
+        filepath = os.path.join('.', filename)
+        with open(filepath, 'wb') as w:
+            w.write(r.content)
+
+
+
+
+
+new_movie= tmdb_downloder("superman")
+new_movie.Get_Imbid()
+new_movie.Poster_Download()
+new_movie.Save_To()
 
 
 
